3d5f5ec8a7de63738384fff1a75c54aade53b9be
satrai-nga <satchandi.rai@newgenapps.com>
2017-06-02 18:20:44 +0530

    some changes

--- messages/index.js
+++ messages/index.js
@@ -1,14 +1,15 @@
-// /*-----------------------------------------------------------------------------
-// This template demonstrates how to use an IntentDialog with a LuisRecognizer to add
-// natural language support to a bot.
-// For a complete walkthrough of creating this type of bot see the article at
-// http://docs.botframework.com/builder/node/guides/understanding-natural-language/
-// -----------------------------------------------------------------------------*/
-"use strict";
-var builder = require("botbuilder");
+// This loads the environment variables from the .env file
+require('dotenv-extended').load();
+
+var builder = require('botbuilder');
 var botbuilder_azure = require("botbuilder-azure");
+var restify = require('restify');
+var Store = require('./store');
+var spellService = require('./spell-service');
 var path = require('path');
 
+
+// Create connector and listen for messages
 var useEmulator = (process.env.NODE_ENV == 'development');
 
 var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
@@ -18,36 +19,7 @@ var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure
     openIdMetadata: process.env['BotOpenIdMetadata']
 });
 
-var bot = new builder.UniversalBot(connector);
-bot.localePath(path.join(__dirname, './locale'));
-
-// Make sure you add code to validate these fields
-var luisAppId = process.env.LuisAppId;
-var luisAPIKey = process.env.LuisAPIKey;
-var luisAPIHostName = process.env.LuisAPIHostName || 'westus.api.cognitive.microsoft.com';
-
-const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' + luisAppId + '&subscription-key=' + luisAPIKey;
-
-// Main dialog with LUIS
-var recognizer = new builder.LuisRecognizer(LuisModelUrl);
-var intents = new builder.IntentDialog({ recognizers: [recognizer] })
-/*
-.matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
-*/
-.matches('greeting', (session, args) => {
-    session.send("hii man!!");
-})
-.matches('weather', (session, args) => {
-    session.send("you asked for weather.." + JSON.stringify(args));
-})
-.onDefault((session) => {
-    session.send('Sorry, I did not understand \'%s\'.', session.message.text);
-});
-
-bot.dialog('/', intents);
-
 if (useEmulator) {
-    var restify = require('restify');
     var server = restify.createServer();
     server.listen(3978, function() {
         console.log('test bot endpont at http://localhost:3978/api/messages');
@@ -56,58 +28,135 @@ if (useEmulator) {
 } else {
     module.exports = { default: connector.listen() }
 }
-// -----------------------------------------------------------------------------------------------------------------
-// var builder = require('botbuilder');
-// var botbuilder_azure = require("botbuilder-azure");
-// var restify = require('restify');
-// var weatherClient = require('./wunderground-client');
-// var path = require('path');
-//
-// var useEmulator = (process.env.NODE_ENV == 'development');
-//
-// var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
-//     appId: process.env['MicrosoftAppId'],
-//     appPassword: process.env['MicrosoftAppPassword'],
-//     stateEndpoint: process.env['BotStateEndpoint'],
-//     openIdMetadata: process.env['BotOpenIdMetadata']
-// });
-// var bot = new builder.BotConnectorBot(connector);
-// // bot.localePath(path.join(__dirname, './locale'));
-//
-// var luisAppId = process.env.LuisAppId;
-// var luisAPIKey = process.env.LuisAPIKey;
-// var luisAPIHostName = process.env.LuisAPIHostName || 'westus.api.cognitive.microsoft.com';
-//
-// const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' + luisAppId + '&subscription-key=' + luisAPIKey;
-//
-// var dialog = new builder.LuisDialog(LuisModelUrl);
-//
-// bot.add('/', dialog);
-// // test
-// dialog.on('builtin.intent.weather.check_weather', [
-//     (session, args, next) => {
-//         var locationEntity = builder.EntityRecognizer.findEntity(args.entities, 'builtin.weather.absolute_location');
-//         if (locationEntity) {
-//             return next({ response: locationEntity.entity });
-//         } else {
-//             builder.Prompts.text(session, 'What location?');
-//         }
-//     },
-//     (session, results) => {
-//         weatherClient.getCurrentWeather(results.response, (responseString) => {
-//             session.send(responseString);
-//         });
-//     }
-// ]);
-//
-// dialog.onDefault(builder.DialogAction.send("I don't understand."));
-//
-// if (useEmulator) {
-//     var server = restify.createServer();
-//     server.listen(3978, function() {
-//         console.log('test bot endpont at http://localhost:3978/api/messages');
-//     });
-//     server.post('/api/messages', connector.listen());
-// } else {
-//     module.exports = { default: connector.listen() }
-// }
+
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send('Sorry, I did not understand \'%s\'. Type \'help\' if you need assistance.', session.message.text);
+});
+var luisAppId = process.env.LuisAppId;
+var luisAPIKey = process.env.LuisAPIKey;
+var luisAPIHostName = process.env.LuisAPIHostName || 'westus.api.cognitive.microsoft.com';
+
+const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' + luisAppId + '&subscription-key=' + luisAPIKey;
+
+// You can provide your own model by specifing the 'LUIS_MODEL_URL' environment variable
+// This Url can be obtained by uploading or creating your model from the LUIS portal: https://www.luis.ai/
+var recognizer = new builder.LuisRecognizer(LuisModelUrl);
+bot.recognizer(recognizer);
+
+bot.dialog('SearchHotels', [
+    function (session, args, next) {
+        session.send('Welcome to the Hotels finder! We are analyzing your message: \'%s\'', session.message.text);
+
+        // try extracting entities
+        var cityEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'builtin.geography.city');
+        var airportEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'AirportCode');
+        if (cityEntity) {
+            // city entity detected, continue to next step
+            session.dialogData.searchType = 'city';
+            next({ response: cityEntity.entity });
+        } else if (airportEntity) {
+            // airport entity detected, continue to next step
+            session.dialogData.searchType = 'airport';
+            next({ response: airportEntity.entity });
+        } else {
+            // no entities detected, ask user for a destination
+            builder.Prompts.text(session, 'Please enter your destination');
+        }
+    },
+    function (session, results) {
+        var destination = results.response;
+
+        var message = 'Looking for hotels';
+        if (session.dialogData.searchType === 'airport') {
+            message += ' near %s airport...';
+        } else {
+            message += ' in %s...';
+        }
+
+        session.send(message, destination);
+
+        // Async search
+        Store
+            .searchHotels(destination)
+            .then(function (hotels) {
+                // args
+                session.send('I found %d hotels:', hotels.length);
+
+                var message = new builder.Message()
+                    .attachmentLayout(builder.AttachmentLayout.carousel)
+                    .attachments(hotels.map(hotelAsAttachment));
+
+                session.send(message);
+
+                // End
+                session.endDialog();
+            });
+    }
+]).triggerAction({
+    matches: 'SearchHotels',
+    onInterrupted: function (session) {
+        session.send('Please provide a destination');
+    }
+});
+
+bot.dialog('ShowHotelsReviews', function (session, args) {
+    // retrieve hotel name from matched entities
+    var hotelEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'Hotel');
+    if (hotelEntity) {
+        session.send('Looking for reviews of \'%s\'...', hotelEntity.entity);
+        Store.searchHotelReviews(hotelEntity.entity)
+            .then(function (reviews) {
+                var message = new builder.Message()
+                    .attachmentLayout(builder.AttachmentLayout.carousel)
+                    .attachments(reviews.map(reviewAsAttachment));
+                session.endDialog(message);
+            });
+    }
+}).triggerAction({
+    matches: 'ShowHotelsReviews'
+});
+
+bot.dialog('Help', function (session) {
+    session.endDialog('Hi! Try asking me things like \'search hotels in Seattle\', \'search hotels near LAX airport\' or \'show me the reviews of The Bot Resort\'');
+}).triggerAction({
+    matches: 'Help'
+});
+
+// Spell Check
+if (process.env.IS_SPELL_CORRECTION_ENABLED === 'true') {
+    bot.use({
+        botbuilder: function (session, next) {
+            spellService
+                .getCorrectedText(session.message.text)
+                .then(function (text) {
+                    session.message.text = text;
+                    next();
+                })
+                .catch(function (error) {
+                    console.error(error);
+                    next();
+                });
+        }
+    });
+}
+
+// Helpers
+function hotelAsAttachment(hotel) {
+    return new builder.HeroCard()
+        .title(hotel.name)
+        .subtitle('%d stars. %d reviews. From $%d per night.', hotel.rating, hotel.numberOfReviews, hotel.priceStarting)
+        .images([new builder.CardImage().url(hotel.image)])
+        .buttons([
+            new builder.CardAction()
+                .title('More details')
+                .type('openUrl')
+                .value('https://www.bing.com/search?q=hotels+in+' + encodeURIComponent(hotel.location))
+        ]);
+}
+
+function reviewAsAttachment(review) {
+    return new builder.ThumbnailCard()
+        .title(review.title)
+        .text(review.text)
+        .images([new builder.CardImage().url(review.image)]);
+}
